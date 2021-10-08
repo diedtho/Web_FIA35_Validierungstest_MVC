1a15acb026582b8109eb921230e700ce7deabb50
Thorsten Diederichs <DiederichsT@bfwob.de>
2021-10-08 12:47:19 +0200

    Projektdateien hinzufügen.

---
+++ Web_FIA35_Validierungstest_MVC/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_FIA35_Validierungstest_MVC.Models;
+
+namespace Web_FIA35_Validierungstest_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {            
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(InsertViewModel insertViewModel)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Insert()
+        {
+            InsertViewModel ivm = new InsertViewModel();
+            ivm.Geburtsdatum = DateTime.Now.AddYears(-18);
+            return View(ivm);
+        }
+
+        [HttpPost]
+        public IActionResult Insert(InsertViewModel eingabe)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(eingabe);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Web_FIA35_Validierungstest_MVC/Models/InsertViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Web_FIA35_Validierungstest_MVC.Models
+{
+    public class InsertViewModel
+    {
+        [Required(ErrorMessage ="Nötig")]
+        [RegularExpression("[a-zA-Z]+")]
+        [StringLength(25, ErrorMessage = "Nachname wird benötigt")]
+        public string Name { get; set; }
+
+        [Required]
+        [RegularExpression("[a-zA-Z]+")]
+        [StringLength(25,ErrorMessage ="Vorname wird benötigt")]
+        public string Vorname { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Geburtsdatum { get; set; }
+
+        [Required]
+        [Range(22,54,ErrorMessage ="Zwischen 22 und 54")]
+        [Display(Name="Schuhgröße")]
+        public int Schuhgroesse { get; set; }
+
+        [Required]
+        [Range(0, 9999999, ErrorMessage = "Zwischen 0 und 9999999 €")]
+        public double Wunschgehalt { get; set; }
+    }
+}
